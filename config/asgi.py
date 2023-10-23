61a7f6b6765ca91a5a285fd52fd951d3057673e6
unohayato <mamidori1p1p@gmail.com>
2023-10-23 18:06:58 +0900

    :sparkles: add consumers, routing

--- config/asgi.py
+++ config/asgi.py
@@ -9,14 +9,24 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+from app.routing import websocket_urlpatterns
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+django_asgi_app = get_asgi_application()
+
+import app.routing
 
 application = ProtocolTypeRouter(
   {
     "http": get_asgi_application(),
-    
+    "websocket": AllowedHostsOriginValidator(
+      AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+    ),
   }
 )
