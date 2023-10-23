f904f2a6101fcd73f4d53d89b14edba968870ef0
unohayato <mamidori1p1p@gmail.com>
2023-10-23 18:30:19 +0900

    :bulb: fix url

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("chat", include("app.urls")),
+    path("chat/", include("app.urls")),
     path("admin/", admin.site.urls),
 ]
