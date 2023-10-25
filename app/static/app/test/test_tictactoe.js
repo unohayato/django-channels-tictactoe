d53c4391c39dff3a9ad7fc70bdaa6d7d502a52a3
unohayato <mamidori1p1p@gmail.com>
2023-10-25 22:52:35 +0900

    :sparkles: add player

--- app/static/app/test/test_tictactoe.js
+++ app/static/app/test/test_tictactoe.js
@@ -2,9 +2,22 @@ const assert = require('assert');
 
 let ttt = require('../src/tictactoe.js');
 
-
+// プレイヤーのテスト
+// 初期値、1->2->1(1ターン目), 1->2(2ターン目の最初)
+let testPlayer = () => {
+  console.log("testPlayer");
+  p1 = ttt.player();
+  p2 = ttt.player(p1);
+  p3 = ttt.player(p2);
+  p4 = ttt.player(p1);
+  assert(p1 === 1);
+  assert(p2 === 2);
+  assert(p3 === 1);
+  assert(p4 === 2);
+};
 
 let runAll = () => {
+  testPlayer();
   console.log('ok')
 };
 
