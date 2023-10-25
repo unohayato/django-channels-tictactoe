9b097149469897e6f9932648d3a6ae0ba75242b3
unohayato <mamidori1p1p@gmail.com>
2023-10-25 22:40:02 +0900

    :fire: delelte hello

--- app/static/app/src/tictactoe.js
+++ app/static/app/src/tictactoe.js
@@ -1,5 +1,4 @@
-let hello = "hello";
 
 module.exports = {
-  hello
+
 };
--- app/static/app/test/test_tictactoe.js
+++ app/static/app/test/test_tictactoe.js
@@ -2,12 +2,9 @@ const assert = require('assert');
 
 let ttt = require('../src/tictactoe.js');
 
-let testHello = () => {
-  assert(ttt.hello === 'hello');
-};
+
 
 let runAll = () => {
-  testHello();
   console.log('ok')
 };
 
